c01a94c954d9c78eabfb71c029ba2968ee991f3b
Andrey <Korobeynkov.kick@gmail.com>
2025-08-28 13:58:36 +0500

    dark 3.2

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -96,8 +96,8 @@ function DashboardPage() {
                   <span className="font-semibold dark:text-white">{totals.positionCount} {getNoun(totals.positionCount, 'позиция', 'позиции', 'позиций')}</span>
                 </div>
                 {totals.totalWeight > 0 && (
-                  <div className="flex items-center text-gray-700 dark:text-white">
-                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <div className="flex items-center text-gray-700 ">
+                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-gray-400 dark:text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
                     </svg>
                     <span className="font-semibold dark:text-white">
