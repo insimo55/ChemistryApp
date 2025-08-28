c189c9f46d1d852aa29691ba60d7c802de0d55f1
Andrey <Korobeynkov.kick@gmail.com>
2025-08-28 12:17:44 +0500

    fix #2 darkmode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,5 @@
-import React, { StrictMode } from 'react';
-import { createRoot, ReactDOM } from 'react-dom/client';
+import React from 'react';
+import { ReactDOM } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import {ThemeProvider} from './context/ThemeProvider.jsx';
